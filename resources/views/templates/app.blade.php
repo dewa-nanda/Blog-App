9d091853ed98158834758f66d29ec90e7abefd91
Dewa Putra Hernanda <dewananda089@gmail.com>
2023-10-10 11:41:56 +0700

    fix design and create template for all file blade

--- resources/views/templates/app.blade.php
+++ resources/views/templates/app.blade.php
@@ -4,11 +4,12 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
-        
+        <title> @yield('title') </title>
+
+        @vite('resources/css/app.css')
     </head>
 
-    <body>
-        
+    <body class="bg-neutral-600">
+        @include('templates.app.header')
     </body>
 </html>
---
+++ resources/views/templates/app/header.blade.php
@@ -0,0 +1,21 @@
+{{-- None	width: 100%;
+sm (640px)	max-width: 640px;
+md (768px)	max-width: 768px;
+lg (1024px)	max-width: 1024px;
+xl (1280px)	max-width: 1280px;
+2xl (1536px)	max-width: 1536px; --}}
+
+<nav class="bg-stone-300 flex justify-between items-center px-3 h-12">
+    <div class="text-2xl">
+        <h1>Logo Here</h1>
+    </div>
+
+    <div class="flex invisible sm:visible">
+        <h3 class="me-2">Login</h3>
+        <h3>Sign up</h3>
+    </div>
+
+    <button class="sm:hidden border-2 rounded-md p-1 bg-stone-700 text-gray-50 ">
+        btn
+    </button>
+</nav>
